dc33a1e5494143beaac98ce08d2c78c96cac97e2
MateuszWalowski <57137126+MateuszWalowski@users.noreply.github.com>
2020-06-03 15:28:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
- function galleries() {
-   const gallery = "http://mawadesign.eu/wordpress/wp-json/wp/v2/gallery?_embed";
-   fetch(gallery)
-     .then(res => res.json())
-     .then(handleData)
+// function galleries() {
+//   const gallery = "http://mawadesign.eu/wordpress/wp-json/wp/v2/gallery?_embed";
+//   fetch(gallery)
+//     .then(res => res.json())
+//     .then(handleData)
 
-   function handleData(posts) {
-     console.log(posts);
-     posts.forEach(showevent);
+//   function handleData(posts) {
+//     console.log(posts);
+//     posts.forEach(showevent);
 
-   }
+//   }
 
-   function showevent(gallery) {
-     console.log(gallery);
-     const template = document.querySelector(".gallery").content;
-     const copy = template.cloneNode(true);
-     copy.querySelector(".galleryname").textContent = gallery.title.rendered;
-     copy.querySelector(".otherworkdescription").innerHTML = gallery.content.rendered;
-
-
-     document.querySelector("main").appendChild(copy);
-
-     data = gallery.images;
-     data.forEach(function (obj) {
-       var img = document.createElement("img");
-       img.src = obj.guid;
-       img.alt = obj.post_title;
-       img.onclick = function NewTab() {
-         window.open(
-           obj.guid, "_blank");
-       }
-       document.querySelector(".hereiswheretheimagesgo").appendChild(img);
+//   function showevent(gallery) {
+//     console.log(gallery);
+//     const template = document.querySelector(".gallery").content;
+//     const copy = template.cloneNode(true);
+//     copy.querySelector(".galleryname").textContent = gallery.title.rendered;
+//     copy.querySelector(".otherworkdescription").innerHTML = gallery.content.rendered;
 
 
-     })
+//     document.querySelector("main").appendChild(copy);
 
-   }
- }
+//     data = gallery.images;
+//     data.forEach(function (obj) {
+//       var img = document.createElement("img");
+//       img.src = obj.guid;
+//       img.alt = obj.post_title;
+//       img.onclick = function NewTab() {
+//         window.open(
+//           obj.guid, "_blank");
+//       }
+//       document.querySelector(".hereiswheretheimagesgo").appendChild(img);
+
+
+//     })
+
+//   }
+// }
 
 function blog()
 
@@ -65,83 +65,7 @@ function blog()
   }
 }
 
-// function portfolio() {
 
-
-//   const gallery = " http://mawadesign.eu/wordpress/wp-json/wp/v2/portfolio_project?_embed";
-//   fetch(gallery)
-//     .then(res => res.json())
-//     .then(handleData)
-
-//   function handleData(posts) {
-//     // console.log(posts);
-//     posts.forEach(showevent);
-
-//   }
-
-//   function showevent(portfolio) {
-//     // console.log(portfolio);
-//     const template = document.querySelector(".portfolio").content;
-//     const copy = template.cloneNode(true);
-//     copy.querySelector(".projectdate").textContent = portfolio.projectdate;
-//     copy.querySelector(".client_name").textContent = portfolio.client_name;
-//     copy.querySelector(".portfoliodesc").innerHTML = portfolio.content.rendered;
-//     copy.querySelector(".featuredimage img").src = portfolio._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url
-//     copy.querySelector(".name").textContent = portfolio.title.rendered;
-
-//     document.querySelector(".portfoliostuff").appendChild(copy);
-
-//     // copy.querySelector("iframe").src = portfolio.content.youtube;
-
-
-//   }
-// }
-
-// var allportfolios = document.querySelectorAll('div')[4].getElementsByClassName('thissetsthepositionofportfolio');
-// console.log(allportfolios);
-
-// allportfolios.forEach(element => console.log(element));
-
-// allportfolios.forEach(function (posts) {
-//   posts.addEventListener('onClick', makeitbig);
-
-//   });
-// document.querySelectorAll("portfoliogallery").addEventListener("click", makeitbig);
-
-
-//   function makeitbig(e){
-// console.log("click");
-// console.log(e.target);
-// var element = document.getElementsByClassName("portfoliogallery");
-// element.classList.add("bigportfolio");
-
-// }
-
-
-//   var allportfolios = document.querySelectorAll("portfoliogallery");
-//   console.log(allportfolios.length);
-
-// for (var i = 0; i < allportfolios.length; i++) {
-//     var self = allportfolios[i];
-
-//     self.addEventListener('click', function (event) {  
-//         // prevent browser's default action
-//         event.preventDefault();
-
-
-//         // call your awesome function here
-//         makeitbig(this); // 'this' refers to the current button on for loop
-//     }, false);
-// }
-
-
-//   function makeitbig(e){
-// console.log("click");
-//     console.log(e.currentTarget);
-//     var element = document.getElementsByClassName("portfoliogallery");
-//     element.classList.add("bigportfolio");
-
-//   }
 
 
 const modal = document.querySelector(".modal-background");
@@ -151,7 +75,7 @@ modal.addEventListener("click", () => {
 
 
 
-fetch(" http://mawadesign.eu/wordpress/wp-json/wp/v2/portfolio_project?_embed")
+fetch("https://mawadesign.eu/wordpress/wp-json/wp/v2/portfolio_project?_embed")
   .then(function (response) {
     return response.json()
   })
@@ -167,9 +91,9 @@ function showData(jsonData) {
 function showSingleDish(portfolio) {
   const template = document.querySelector(".portfolio").content;
   const copy = template.cloneNode(true);
-  copy.querySelector(".featuredimage img").src = portfolio._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url
+  copy.querySelector(".featuredimage img").src = portfolio._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
   copy.querySelector(".name").textContent = portfolio.title.rendered;
-  copy.querySelector("button").addEventListener("click", () => {
+  copy.querySelector(".featuredimage").addEventListener("click", () => {
     console.log("click", portfolio)
     fetch(`http://mawadesign.eu/wordpress/wp-json/wp/v2/portfolio_project/${portfolio.id}`)
       .then(res => res.json())
@@ -182,15 +106,56 @@ function showSingleDish(portfolio) {
 function showDetails(data) {
   console.log(data);
   modal.classList.remove("hide");
-  // modal.querySelector(".portfoliogallery").textContent = "just some stuff to see if it works at all ";
 
   modal.querySelector(".modal-name").textContent = data.title.rendered;
 
-  if (data.youtube !== null && data.youtube !== '') {
+  if (data.youtube) {
     modal.querySelector("iframe").src = data.youtube;
+    modal.querySelector("iframe").style.display = "block";
+    console.log("fetching " + data.youtube)
+
   } else {
     modal.querySelector("iframe").style.display = "none";
+
   }
 
 
-}
+  if (data.gallery) {
+    modal.querySelector(".modal-gallery").style.display = "flex";
+
+    modal.querySelector(".modal-gallery").innerHTML = "";;
+    images  = data.gallery
+    console.log(images);
+    images.forEach(function (obj) {
+        var img = document.createElement("img");
+        img.src = obj.guid;
+        img.alt = obj.post_title;
+        img.onclick = function NewTab() {
+          window.open(
+            obj.guid, "_blank");
+    }
+          document.querySelector(".modal-gallery").appendChild(img);
+        } )
+      }
+
+    
+    else {
+      modal.querySelector(".modal-gallery").innerHTML = "";;
+      modal.querySelector(".modal-gallery").style.display = "none";
+
+    }
+
+
+
+    //     data = gallery.images;
+    //     data.forEach(function (obj) {
+    //       var img = document.createElement("img");
+    //       img.src = obj.guid;
+    //       img.alt = obj.post_title;
+    //       img.onclick = function NewTab() {
+    //         window.open(
+    //           obj.guid, "_blank");
+    //       }
+    //       document.querySelector(".hereiswheretheimagesgo").appendChild(img);
+
+  }
